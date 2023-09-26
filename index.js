0c2171be219bed8c33272b631817b9adb255503f
FaithAdline <adlinefaith@gmail.com>
2023-09-26 11:57:09 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Code your solutions in this file
+// code your solutions here
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    debugger;
+}
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+function writeCards(names, event) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouMessages;
+  }
+  
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  const names = ["Chelsea", "Olive", "Alvin"];
+  const event = "birthday";
+  const thanksall = writeCards(names, event);
+  console.log(thanksall);
+  
+  const countDownNumber = 6;
+  countDown(countDownNumber);
+  
+  
